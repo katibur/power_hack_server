886bf3224d25a0b3b669fdff216d0c0ec322db9e
S M Katibur Rahman <katiburrahmansany@gmail.com>
2023-01-30 03:06:49 +0600

    almost done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 require("dotenv").config();
 
@@ -18,6 +18,22 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("unauthorized access");
+  }
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     const billingCollection = client.db("power_hack").collection("billingList");
@@ -25,9 +41,53 @@ async function run() {
     app.post("/jwt", (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "30d",
+        expiresIn: "100d",
       });
       res.send({ token });
+    });
+
+    app.post("/add-billing", async (req, res) => {
+      const query = req.body;
+      console.log(query);
+      const result = await billingCollection.insertOne(query);
+      res.send(result);
+    });
+
+    app.get("/billing-list", async (req, res) => {
+      const query = {};
+      const billingList = await billingCollection.find(query).toArray();
+      res.send(billingList);
+    });
+    app.put("/update-billing", async (req, res) => {
+      const id = req.query.id;
+      console.log(id);
+      const updatedData = req.body;
+      console.log(updatedData);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: updatedData.name,
+          email: updatedData.email,
+          phone: updatedData.phone,
+          amount: updatedData.amount,
+        },
+      };
+      console.log(filter, updateDoc);
+      const result = await billingCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    app.delete("/delete-billing/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      console.log(filter);
+      const result = await billingCollection.deleteOne(filter);
+      res.send(result);
     });
   } finally {
   }
